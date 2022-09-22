328cc6612d0f378068f6363ce336db6dffd469ba
Diego Santellan <diego.santellan2001@gmail.com>
2022-09-21 23:49:24 -0300

    modificacion navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,26 @@
 function Navbar() {
   return (
     <>
-        <nav class="navbar bg-light">
+        <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
             <div class="container-fluid">
-                <a class="navbar-brand">Navbar</a>
-                <form class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                    <button class="btn btn-outline-success" type="submit">Search</button>
-                </form>
-            </div>
+                <a class="navbar-brand" href="#">Urban Culture</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>   
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link disabled">Productos</a>
+                        </li>
+                    </ul>
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search"/>
+                        <button id="btnBusqueda"  class="btn btn-outline-warning" type="submit">Buscar</button>
+                    </form>
+                </div>
+           
+            </div>  
         </nav>
-        
     </>
 
   );
